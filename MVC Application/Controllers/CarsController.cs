2f0684a13c27a10fde116a0952c45271404d19e5
Srinivas Vandamsetty <vandamsetty.saisrinivas@gmail.com>
2024-12-17 19:26:21 +0530

    Add project files.

---
+++ MVC Application/Controllers/CarsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Application.Controllers
+{
+    public class CarsController : Controller
+    {
+        public IActionResult Index()
+        {
+            string[] carsNames = { "Toyota", "Audi", "Tata", "BMW" };
+            var cars = new List<string>(carsNames);
+            return View(cars);
+        }
+    }
+}
