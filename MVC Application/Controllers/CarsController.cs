7d4bfd3ab2e891f118c4d5201d39e7770932168c
Srinivas Vandamsetty <vandamsetty.saisrinivas@gmail.com>
2024-12-18 16:23:15 +0530

    Updated the changes in MVC App

--- MVC Application/Controllers/CarsController.cs
+++ MVC Application/Controllers/CarsController.cs
@@ -1,13 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Application.Models;
 
 namespace MVC_Application.Controllers
 {
-    public class CarsController : Controller
+    // Base route for the controller
+    [Route("car")]
+    public class CarController : Controller
     {
-        public IActionResult Index()
+        // Route for the Details action
+        [Route("details")]
+        public IActionResult Details()
         {
-            string[] carsNames = { "Toyota", "Audi", "Tata", "BMW" };
-            var cars = new List<string>(carsNames);
+            // Simulating a list of cars fetched from the database
+            var cars = new List<Car>
+            {
+                new Car { Make = "Toyota", Model = "Camry", Year = 2021, Price = 25000 },
+                new Car { Make = "Honda", Model = "Civic", Year = 2020, Price = 22000 },
+                new Car { Make = "Ford", Model = "Mustang", Year = 2022, Price = 35000 },
+                new Car { Make = "Chevrolet", Model = "Malibu", Year = 2021, Price = 24000 },
+                new Car { Make = "BMW", Model = "3 Series", Year = 2021, Price = 40000 }
+            };
+
+            // Passing the model to the view
             return View(cars);
         }
     }
---
+++ MVC Application/Models/CarsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_Application.Models
+{
+    public class Car
+    {
+        public string? Make { get; set; } 
+        public string? Model { get; set; }
+        public int Year { get; set; } 
+        public decimal Price { get; set; }
+    }
+}
